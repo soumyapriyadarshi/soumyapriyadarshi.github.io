6e22ae5861eeec959fcdc06c61b6c30a2de89e60
soumyaweb2006 <soumyanayak052006@gmail.com>
2025-11-08 22:48:16 +0530

    adding js file

---
+++ app.js
@@ -0,0 +1,8 @@
+// selction nessessary elements to make the website good
+const light =  document.querySelector(".light");
+
+
+// adding event listener to each element
+light.addEventListener("click", () => {
+    document.body.classList.toggle("active");
+})
