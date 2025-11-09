a75d2c69267d4a250cb43a62e1d745d0c5a541c2
soumyaweb2006 <soumyanayak052006@gmail.com>
2025-11-09 09:58:42 +0530

    js

--- app.js
+++ app.js
@@ -5,5 +5,4 @@ const light =  document.querySelector(".light");
 // adding event listener to each element
 light.addEventListener("click", () => {
     document.body.classList.toggle("active");
-    light.classList.toggle("active");
 })
